4ef993fa433c9798cfc2daeff9582b16807972ea
Ryan Hinkle <hinkle.ryan97@gmail.com>
2021-11-23 15:48:13 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const body = document.body;
+let newHeader = document.createElement("h1");
+newHeader.textContent = "Ryan is the champion";
+newHeader.nodeName = "h1#victory";
+newHeader.id = 'victory';
+body.append(newHeader);
