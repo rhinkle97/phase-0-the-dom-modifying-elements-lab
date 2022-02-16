6b6dc8d4134a2cfa081b35ae016d821e21248e4b
Ryan Hinkle <hinkle.ryan97@gmail.com>
2022-02-15 19:52:42 -0800

    commit tree 7289a94f3a76785ce7b97be401778f8bb6f9078c

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Write your code here!
-const body = document.body;
-let newHeader = document.createElement("h1");
-newHeader.textContent = "Ryan is the champion";
-newHeader.nodeName = "h1#victory";
-newHeader.id = 'victory';
-body.append(newHeader);
+// const body = document.body;
+// let newHeader = document.createElement("h1");
+// newHeader.textContent = "Ryan is the champion";
+// newHeader.nodeName = "h1#victory";
+// newHeader.id = 'victory';
+// body.append(newHeader);
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'Ryan is the champion';
